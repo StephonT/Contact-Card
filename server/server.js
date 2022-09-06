7d564e3574fe87f5e64d646bd7e06cdf6d0fcec6
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-09-06 16:57:21 -0400

    moved back end code to server directory

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//enables the ability to use the client folder
+app.use(express.static('../client/'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
+
